e2e31e2516df23c38588b17fd035ac8f4bba48b7
vijaysarma1007 <svmsarma53@gmail.com>
2026-03-14 23:09:04 +0530

    added turbo fishing in generics

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", identity::<i32>(5));
+    println!("{}", identity::<String>(String::from("Random text")));
+
+    make_tuple::<&str, i32>("hello", 3);
 }
 
-fn identity(){}
+fn make_tuple<T, U>(first:T, second:U) -> (T, U) {
+    (first, second)
+}
+
+fn identity<T>(value: T) -> T {
+    value
+}
+ 
